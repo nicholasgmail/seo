f8dad7f300fe341a38cade0e5a1829ff89d7516d
Mark-1985 <alushyn5@gmail.com>
2020-07-28 14:31:57 +0300

    footer-header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,3 +4,15 @@ function f() {
     return console.log('hello')
 }
 f;
+$(document).ready(function () {
+
+    // hover for the dropdown
+    $("ul li.dropdown").hover(
+        function () {
+            $(this).find(".dropdown-menu").addClass("show");
+        },
+        function () {
+            $(this).find(".dropdown-menu").removeClass("show");
+        }
+    );
+});
