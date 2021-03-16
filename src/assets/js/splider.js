f686f865f64a72738a8be17f6a484c2271a48652
Mark-1985 <alushyn5@gmail.com>
2021-03-16 17:39:53 +0200

    add sct-development-cases

--- src/assets/js/splider.js
+++ src/assets/js/splider.js
@@ -5,10 +5,24 @@ new Splide('#website-promotion-splide', {
    pagination: false,
    width: '100%',
    perPage: 1,
-   padding: {
-      left: '2rem',
-      right: '2rem',
-   },
+   breakpoints: {
+      576: {
+         perPage: 1,
+         gap: 8,
+         padding: {
+            left: '2rem',
+            right: '2rem',
+         }
+      },
+   }
+}).mount();
+new Splide('#development-cases-splide', {
+   type: 'loop',
+   lazyLoad: true,
+   autoplay: false,
+   pagination: false,
+   width: '100%',
+   perPage: 1,
    breakpoints: {
       576: {
          perPage: 1,
