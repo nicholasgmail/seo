ee2a478c8756716020f11afe7c5b1b4ab3716771
Mark-1985 <alushyn5@gmail.com>
2021-03-16 11:14:29 +0200

    add sct-website-promotion

--- src/assets/js/splider.js
+++ src/assets/js/splider.js
@@ -1,4 +1,25 @@
-
+new Splide('#website-promotion-splide', {
+   type: 'loop',
+   lazyLoad: true,
+   autoplay: false,
+   pagination: false,
+   width: '100%',
+   perPage: 1,
+   padding: {
+      left: '2rem',
+      right: '2rem',
+   },
+   breakpoints: {
+      576: {
+         perPage: 1,
+         gap: 8,
+         padding: {
+            left: '2rem',
+            right: '2rem',
+         }
+      },
+   }
+}).mount();
 new Splide('#splide-header', {
    type: 'loop',
    autoplay: true,
