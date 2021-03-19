61bda1b2844ee832fc8d7198f5d74ac74dede347
Mark-1985 <alushyn5@gmail.com>
2021-03-19 13:28:29 +0200

    update slider-cases

--- src/assets/js/splider-cases.js
+++ src/assets/js/splider-cases.js
@@ -11,40 +11,46 @@ function addStyleArrow() {
 }
 addStyleArrow();
 
-new Splide('#website-promotion-splide', {
-   type: 'loop',
-   lazyLoad: true,
-   autoplay: false,
-   pagination: false,
-   width: '100%',
-   perPage: 1,
-   breakpoints: {
-      576: {
-         perPage: 1,
-         gap: 8,
-         padding: {
-            left: '2rem',
-            right: '2rem',
-         }
-      },
-   }
-}).mount();
-new Splide('#development-cases-splide', {
-   type: 'loop',
-   lazyLoad: true,
-   autoplay: false,
-   pagination: false,
-   width: '100%',
-   perPage: 1,
-   breakpoints: {
-      576: {
-         perPage: 1,
-         gap: 8,
-         padding: {
-            left: '2rem',
-            right: '2rem',
-         }
-      },
-   }
-}).mount();
+const $splWebProm = document.getElementById('website-promotion-splide');
+const $splDevCases = document.getElementById('development-cases-splide');
 
+if ($splWebProm) {
+   new Splide('#website-promotion-splide', {
+      type: 'loop',
+      lazyLoad: true,
+      autoplay: false,
+      pagination: false,
+      width: '100%',
+      perPage: 1,
+      breakpoints: {
+         576: {
+            perPage: 1,
+            gap: 8,
+            padding: {
+               left: '2rem',
+               right: '2rem',
+            }
+         },
+      }
+   }).mount();
+}
+if ($splDevCases) {
+   new Splide('#development-cases-splide', {
+      type: 'loop',
+      lazyLoad: true,
+      autoplay: false,
+      pagination: false,
+      width: '100%',
+      perPage: 1,
+      breakpoints: {
+         576: {
+            perPage: 1,
+            gap: 8,
+            padding: {
+               left: '2rem',
+               right: '2rem',
+            }
+         },
+      }
+   }).mount();
+}
